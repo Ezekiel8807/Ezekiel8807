70d1920e0d3bf18ecd87f7f64bdb4cc10f9ca01c
Ezekiel8807 <Ayeoluwaseyi@gmail.com>
2025-01-14 19:32:12 +0100

    fix bug against deployment'

--- src/components/AchieveCard.tsx
+++ src/components/AchieveCard.tsx
@@ -3,11 +3,11 @@ type AchieveCardProbs = {
   content: string;
 };
 
-export default function AchieveCard({ len, content }: AchieveCardProbs) {
+export default function AchieveCard(props: AchieveCardProbs) {
   return (
     <div className="w-[200px] rounded-[15px] p-5 m-5 bg-white">
-      <h1 className="font-black text-3xl text-center">{len}</h1>
-      <h5 className="font-black text-sm text-center">{content}</h5>
+      <h1 className="font-black text-3xl text-center">{props.len}</h1>
+      <h5 className="font-black text-sm text-center">{props.content}</h5>
     </div>
   );
 }
